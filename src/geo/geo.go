aa3ebbc03ff698b9e33baf44711064c46c9fe20b
Niakr1s <pavel2188@gmail.com>
2020-06-10 16:23:31 +0300

    Pos, Radius constructors

--- src/geo/geo.go
+++ src/geo/geo.go
@@ -9,6 +9,12 @@ type Pos struct {
 	X, Y float64
 }
 
-type Radius struct {
-	R float64
+func NewPos(x, y float64) Pos {
+	return Pos{x, y}
+}
+
+type Radius float64
+
+func NewRadius(r float64) Radius {
+	return Radius(r)
 }
